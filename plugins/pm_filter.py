006d51df6b94815f036cf4b02255db5bc2093d74
Yadhu <102352346+yedhukrishnanu19@users.noreply.github.com>
2022-05-06 09:58:29 +0530

    Update pm_filter.py
    😃

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -1408,37 +1408,25 @@ async def auto_filter(client, msg, spoll=False):
         try:
 
             hehe = await message.reply_photo(photo=imdb.get('poster'), caption=cap, reply_markup=InlineKeyboardMarkup(btn))
-
             await asyncio.sleep(300)
-
-            await hehe.delete()            
-
+            await hehe.delete()
+            await message.reply_text(text=f"⚙️ Fɪʟᴛᴇʀ Fᴏʀ {search} Cʟᴏꜱᴇᴅ 🗑️", disable_notification = True)
         except (MediaEmpty, PhotoInvalidDimensions, WebpageMediaEmpty):
-
             pic = imdb.get('poster')
-
             poster = pic.replace('.jpg', "._V1_UX360.jpg")
-
             hmm = await message.reply_photo(photo=poster, caption=cap, reply_markup=InlineKeyboardMarkup(btn))
-
-            await asyncio.sleep(300)            
-
+            await asyncio.sleep(300)
+            await hmm.edit_text(text=f"⚙️ Fɪʟᴛᴇʀ Fᴏʀ {search} Cʟᴏꜱᴇᴅ 🗑️", disable_notification = True)
         except Exception as e:
-
             logger.exception(e)
-
-            fek = await message.reply_text(text=cap, disable_web_page_preview=True, reply_markup=InlineKeyboardMarkup(btn))
-
-            await asyncio.sleep(300)            
-
+            fek = await message.reply_photo(photo="https://telegra.ph/file/82b5bbbab6d5e5593b6b2.jpg", caption=cap, reply_markup=InlineKeyboardMarkup(btn))
+            await asyncio.sleep(300)
+            await fek.edit_text(text=f"⚙️ Fɪʟᴛᴇʀ Fᴏʀ {search} Cʟᴏꜱᴇᴅ 🗑️")
     else:
-
-        fuk = await message.reply_text(text=cap, disable_web_page_preview=True, reply_markup=InlineKeyboardMarkup(btn))
-
+        fuk = await message.reply_photo(photo="https://telegra.ph/file/8b42f6caf6ef5fd76766f.jpg", caption=cap, reply_markup=InlineKeyboardMarkup(btn))
         await asyncio.sleep(300)
-
         await fuk.delete()
-
+        await message.reply_text(text=f"⚙️ Fɪʟᴛᴇʀ Fᴏʀ {search} Cʟᴏꜱᴇᴅ 🗑️")
 
 async def advantage_spell_chok(msg):
     query = re.sub(
