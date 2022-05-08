38658e9a148fe82eb42776bad6bf51ef95d57462
Yadhu <102352346+yedhukrishnanu19@users.noreply.github.com>
2022-05-08 07:45:31 +0530

    Update pm_filter.py
    Yj

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -909,20 +909,6 @@ async def cb_handler(client: Client, query: CallbackQuery):
             ],[
             InlineKeyboardButton('⚚ 𝙱𝙰𝙲𝙺 ⚚', callback_data='start')
         ]]
-        reply_markup = InlineKeyboardMarkup(buttons)       
-        await query.message.edit_text(
-            text="□■"
-        )
-        await query.message.edit_text(
-            text="□■□■"
-        )
-        await query.message.edit_text(
-            text="□■□■□■□"
-        )
-        await query.message.edit_text(                     
-            text=script.HELP_TXT.format(query.from_user.mention),
-            reply_markup=reply_markup,
-            parse_mode='html'
         reply_markup = InlineKeyboardMarkup(buttons)
         await query.answer("𝖶𝖾𝗅𝖼𝗈𝗆𝖾 𝗍𝗈 𝗆𝗒 𝖧𝖾𝗅𝗉 𝗆𝗈𝖽𝗎𝗅𝖾")
         await query.message.edit_text(
